bbec1789fd9277650939c6648688b234ed7e8e82
유찬우 <dbcksdnqkqh@naver.com>
2025-04-30 10:26:18 +0900

    마리아디비 포트수정

--- src/main/java/springChatbot/service/ChatService.java
+++ src/main/java/springChatbot/service/ChatService.java
@@ -13,8 +13,8 @@ import java.util.List;
 @Service
 public class ChatService {
 
-    // @Value("${fastapi.url:http://localhost:8000}")
-    @Value("${fastapi.url:http://172.23.119.212:8000}")
+    @Value("${fastapi.url:http://localhost:8000}")
+    // @Value("${fastapi.url:http://172.23.119.212:8000}")
     private String fastApiUrl;
 
     private final RestTemplate restTemplate;
